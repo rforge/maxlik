d59f80293f59d8945a751d117f685d0d08f70766
suapm095 <suapm095@af626d81-0c90-417d-8daa-7afe02cc634b>
2008-01-03 11:28:07 +0000

    splitted the micEcon package into three packages: maxLik, sampleSelection, and micEcon; adjusted DESCRIPTION files (package title, dependencies, description, author, increased version number to 0.5-0, updated date), INDEX files, <pkg-name>-internal.Rd files, <pkg-name>-deprecated.Rd file, example in maxLik/man/nObs.Rd, and example in maxLik/man/logLikValue.Rd
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/maxlik@511 af626d81-0c90-417d-8daa-7afe02cc634b

--- trunk/maxLik/R/vcov.maxLik.R
+++ trunk/maxLik/R/vcov.maxLik.R
@@ -1,12 +1,3 @@
-## probit
-vcov.probit <- function(object, ...) {
-  result <- vcov.maxLik( object )
-  if(!is.null(result))
-      rownames( result ) <- colnames( result ) <- names( object$estimate )
-  return( result )
-}
-
-
 ## maxLik
 vcov.maxLik <- function(object, ...) {
    ## if exists $varcovar, take it
