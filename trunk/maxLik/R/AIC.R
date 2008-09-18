258a0231e8ec8003952eab9002cc42ade6015fda
suapm095 <suapm095@af626d81-0c90-417d-8daa-7afe02cc634b>
2008-09-18 08:42:14 +0000

    removed definition of (S3) generic function AIC (it is already defined in the stats package)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/maxlik@966 af626d81-0c90-417d-8daa-7afe02cc634b

--- trunk/maxLik/R/AIC.R
+++ trunk/maxLik/R/AIC.R
@@ -1,7 +1,3 @@
 ## Akaike (and other) information criteria
-AIC <- function( object, ... )
-    ## Number of iterations for iterative models
-    UseMethod("AIC")
-
 AIC.maxLik <- function(object, ..., k = 2)
     -2*logLik(object) + k*nParam(object, free=TRUE)
