66fdf70470f784fdbaa47ab099232d3a96f42291
otoomet <otoomet@af626d81-0c90-417d-8daa-7afe02cc634b>
2008-09-22 07:08:12 +0000

    condiNumber: added print.level=1 option
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/maxlik@971 af626d81-0c90-417d-8daa-7afe02cc634b

--- branches/condinumber/maxLik/R/condiNumber.R
+++ branches/condinumber/maxLik/R/condiNumber.R
@@ -4,10 +4,11 @@
 condiNumber <- function(x, ...)
     UseMethod("condiNumber")
 
-condiNumber.default <- function(x, exact=FALSE, norm=FALSE, ...) {
+condiNumber.default <- function(x, exact=FALSE, norm=FALSE, print.level=1, ...) {
    ## x:  a matrix, condition number of which are to be printed
    ## exact: whether the condition number have to be exact or approximated (see 'kappa')
    ## norm: whether to normalise the matrix' columns.
+   ## print.level: whether to print the condition numbers while calculating.  Useful for interactive testing. 
    if(dim(x)[2] > dim(x)[1]) {
       warning(paste(dim(x)[1], "rows and", dim(x)[2], "columns, use transposed matrix"))
       x <- t(x)
@@ -19,7 +20,9 @@ condiNumber.default <- function(x, exact=FALSE, norm=FALSE, ...) {
     }
     for(i in seq(length=ncol(x))) {
         m <- x[,1:i]
-        cat(colnames(x)[i], "\t", cn[i] <- kappa(m, exact=exact), "\n")
+        cn[i] <- kappa(m, exact=exact)
+        if(print.level > 0)
+            cat(colnames(x)[i], "\t", cn[i], "\n")
     }
    names(cn) <- colnames(x)
    invisible(cn)
