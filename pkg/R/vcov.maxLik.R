a6b3b656c17693fa094cab5a993495d16b807eba
otoomet <otoomet@af626d81-0c90-417d-8daa-7afe02cc634b>
2008-10-29 18:55:57 +0000

    vcov.maxLik() uses better singularity check
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/maxlik@1002 af626d81-0c90-417d-8daa-7afe02cc634b

--- pkg/R/vcov.maxLik.R
+++ pkg/R/vcov.maxLik.R
@@ -1,12 +1,13 @@
 ## maxLik
-vcov.maxLik <- function(object, ...) {
+vcov.maxLik <- function(object, eigentol=1e-12, ...) {
    ## if exists $varcovar, take it
    if(!is.null(object$varcovar))
        return(object$varcovar)
    ## otherwise invert hessian
    activePar <- activePar(object)
-   if(min(abs(eigen(hessian(object)[activePar,activePar],
-                    symmetric=TRUE, only.values=TRUE)$values)) > 1e-6) {
+   hess <- hessian(object)[activePar, activePar] 
+   hessev <- abs(eigen(hess, symmetric=TRUE, only.values=TRUE)$values)
+   if(min(hessev) > (eigentol*max(hessev))) {
       varcovar <- matrix(0, nParam(object), nParam(object))
       varcovar[activePar,activePar] <- solve(-hessian(object)[activePar,activePar])
    }
