3e86cf759ba9da3defbb837d7b3151512da52e06
arne <arne@af626d81-0c90-417d-8daa-7afe02cc634b>
2009-06-02 10:49:33 +0000

    maxBHHH() returns a more helpful error message now if (a) the gradients (argument 'grad') are not provided by the user and the log-likelihood function (argument 'fn') returns a single value (rather than a numeric vector with at least as many elements as the number of parameters, where each element corresponds to the log-likelihood value corresponding to an individual observation) or (b) the gradient function (argument 'grad') returns only a single vector with elements for each parameter (rather than a numeric matrix with at least as many rows as the number of parameters, where each row must correspond to the gradients of the log-likelihood function of an individual observation)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/maxlik@1023 af626d81-0c90-417d-8daa-7afe02cc634b

--- pkg/R/maxBHHH.R
+++ pkg/R/maxBHHH.R
@@ -4,9 +4,35 @@ maxBHHH <- function(fn, grad=NULL, hess=NULL,
                     iterlim=100,
                     ...) {
    ## hess:   Hessian, not used, for compatibility with the other methods
+
+   ## checking the log likelihodd function
+   testVal <- fn( start, ... )
+   if( length( testVal ) < length( start ) ) {
+      stop( "if the gradients (argument 'grad') are not provided by the user,",
+         " the BHHH method requires that the log-likelihood function",
+         " (argument 'fn') returns a numeric vector with at least as many",
+         " elements as the number of parameters (", length( start ), "),",
+         " where each element must be the log-likelihood value corresponding",
+         " to an individual observation" )
+   }
+   rm( testVal )
+
+   ## checking the gradient function
+   if( !is.null( grad ) ) {
+      testGrad <- grad( start, ... )
+      if( nrow( matrix( testGrad ) ) < length( start ) ) {
+         stop( "the BHHH method requires that the gradient function",
+            " (argument 'grad') returns a numeric matrix with at least as many",
+            " rows as the number of parameters (", length( start ), "),",
+            " where each row must correspond to the gradients",
+            " of the log-likelihood function of an individual observation" )
+      }
+      rm( testGrad )
+   }
+
+   ## Save the value of gradient and use it later for hessian
    gradVal <- NULL
-   # Save the value of gradient and use it later for hessian
-   # Hessian must be called with the same parameter as gradient
+
    gradient <- function(theta, ...) {
       if(!is.null(grad)) {
          g <- grad(theta, ...)
