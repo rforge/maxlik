869993d8ba19f57c33a8467070ae2b2f0aea06bc
arne <arne@af626d81-0c90-417d-8daa-7afe02cc634b>
2009-07-20 07:42:00 +0000

    added test script (and corresponding output file) that fits a normal distribution in several different ways
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/maxlik@1051 af626d81-0c90-417d-8daa-7afe02cc634b

---
+++ pkg/tests/fitNormalDist.R
@@ -0,0 +1,226 @@
+# load the 'maxLik' package
+library(maxLik)
+
+## data to fit a normal distribution
+# set seed for pseudo random numbers
+set.seed( 123 )
+# generate a variable of exponentially distributed values
+x <- rnorm( 100, 1, 2 )
+
+## log likelihood function
+llf <- function( param ) {
+   mu <- param[ 1 ]
+   sigma <- param[ 2 ]
+   N <- length( x )
+   llValue <- -0.5 * N * log( 2 * pi ) - N * log( sigma ) -
+      0.5 * sum( ( x - mu )^2 / sigma^2 )
+   return( llValue )
+}
+
+## log likelihood function (individual observations)
+llfInd <- function( param ) {
+   mu <- param[ 1 ]
+   sigma <- param[ 2 ]
+   llValues <- -0.5 * log( 2 * pi ) - log( sigma ) -
+      0.5 * ( x - mu )^2 / sigma^2
+   return( llValues )
+}
+
+## function to calculate analytical gradients
+gf <- function( param ) {
+   mu <- param[ 1 ]
+   sigma <- param[ 2 ]
+   N <- length( x )
+   llGrad <- c( sum( ( x - mu ) / sigma^2 ),
+      - N / sigma + sum( ( x - mu )^2 / sigma^3 ) )
+   return( llGrad )
+}
+
+## function to calculate analytical gradients (individual observations)
+gfInd <- function( param ) {
+   mu <- param[ 1 ]
+   sigma <- param[ 2 ]
+   llGrads <- cbind( ( x - mu ) / sigma^2,
+      - 1 / sigma + ( x - mu )^2 / sigma^3 )
+   return( llGrads )
+}
+
+## function to calculate analytical Hessians
+hf <- function( param ) {
+   mu <- param[ 1 ]
+   sigma <- param[ 2 ]
+   N <- length( x )
+   llHess <- matrix( c(
+      N * ( - 1 / sigma^2 ),
+      sum( - 2 * ( x - mu ) / sigma^3 ),
+      sum( - 2 * ( x - mu ) / sigma^3 ),
+      N / sigma^2 + sum( - 3 * ( x - mu )^2 / sigma^4 ) ),
+      nrow = 2, ncol = 2 )
+   return( llHess )
+}
+
+
+## NR method
+ml <- maxLik( llf, start = c( 0, 1 ) )
+print( ml )
+summary( ml )
+activePar( ml )
+AIC( ml )
+coef( ml )
+condiNumber( ml )
+hessian( ml )
+logLik( ml )
+maximType( ml )
+nIter( ml )
+nObs( ml )
+nParam( ml )
+returnCode( ml )
+returnMessage( ml )
+vcov( ml )
+logLik( summary( ml ) )
+mlInd <- maxLik( llfInd, start = c( 0, 1 ) )
+summary( ml )
+all.equal( ml, mlInd )
+
+# with analytical gradients
+mlg <- maxLik( llf, gf, start = c( 0, 1 ) )
+summary( mlg )
+all.equal( ml, mlg )
+mlgInd <- maxLik( llfInd, gfInd, start = c( 0, 1 ) )
+all.equal( mlInd, mlgInd )
+all.equal( mlg, mlgInd )
+
+# with analytical gradients and Hessians
+mlgh <- maxLik( llf, gf, hf, start = c( 0, 1 ) )
+all.equal( mlg, mlgh )
+
+
+## BHHH method
+mlBHHH <- maxLik( llfInd, start = c( 0, 1 ), method = "BHHH" )
+print( mlBHHH )
+summary( mlBHHH )
+activePar( mlBHHH )
+AIC( mlBHHH )
+coef( mlBHHH )
+condiNumber( mlBHHH )
+hessian( mlBHHH )
+logLik( mlBHHH )
+maximType( mlBHHH )
+nIter( mlBHHH )
+nObs( mlBHHH )
+nParam( mlBHHH )
+returnCode( mlBHHH )
+returnMessage( mlBHHH )
+vcov( mlBHHH )
+logLik( summary( mlBHHH ) )
+all.equal( ml, mlBHHH )
+
+# with analytical gradients
+mlgBHHH <- maxLik( llfInd, gfInd, start = c( 0, 1 ), method = "BHHH" )
+summary( mlgBHHH )
+all.equal( mlBHHH, mlgBHHH )
+all.equal( mlg, mlgBHHH )
+
+# with unused Hessian
+mlghBHHH <- maxLik( llfInd, gfInd, hf, start = c( 0, 1 ), method = "BHHH" )
+all.equal( mlgBHHH, mlghBHHH )
+
+
+## BFGS method
+mlBFGS <- maxLik( llf, start = c( 0, 1 ), method = "BFGS" )
+print( mlBFGS )
+summary( mlBFGS )
+activePar( mlBFGS )
+AIC( mlBFGS )
+coef( mlBFGS )
+condiNumber( mlBFGS )
+hessian( mlBFGS )
+logLik( mlBFGS )
+maximType( mlBFGS )
+nIter( mlBFGS )
+nObs( mlBFGS )
+nParam( mlBFGS )
+returnCode( mlBFGS )
+returnMessage( mlBFGS )
+# vcov( mlBFGS )
+logLik( summary( mlBFGS ) )
+all.equal( ml[-8], mlBFGS )
+mlIndBFGS <- maxLik( llfInd, start = c( 0, 1 ), method = "BFGS" )
+summary( mlIndBFGS )
+all.equal( mlBFGS, mlIndBFGS )
+
+# with analytical gradients
+mlgBFGS <- maxLik( llf, gf, start = c( 0, 1 ), method = "BFGS" )
+summary( mlgBFGS )
+all.equal( mlBFGS, mlgBFGS )
+all.equal( mlg[-8], mlgBFGS )
+mlgIndBFGS <- maxLik( llfInd, gfInd, start = c( 0, 1 ), method = "BFGS" )
+all.equal( mlgBFGS, mlgIndBFGS )
+
+# with unused Hessian
+mlghBFGS <- maxLik( llf, gf, hf, start = c( 0, 1 ), method = "BFGS" )
+all.equal( mlgBFGS, mlghBFGS )
+
+
+## NM method
+mlNM <- maxLik( llf, start = c( 0, 1 ), method = "NM" )
+print( mlNM )
+summary( mlNM )
+activePar( mlNM )
+AIC( mlNM )
+coef( mlNM )
+condiNumber( mlNM )
+hessian( mlNM )
+logLik( mlNM )
+maximType( mlNM )
+nIter( mlNM )
+nObs( mlNM )
+nParam( mlNM )
+returnCode( mlNM )
+returnMessage( mlNM )
+# vcov( mlNM )
+logLik( summary( mlNM ) )
+all.equal( ml[-8], mlNM )
+
+# with unused analytical gradients
+mlgNM <- maxLik( llf, gf, start = c( 0, 1 ), method = "NM" )
+summary( mlgNM )
+all.equal( mlNM, mlgNM )
+
+# with unused analytical gradients and Hessian
+mlghNM <- maxLik( llf, gf, hf, start = c( 0, 1 ), method = "NM" )
+all.equal( mlgNM, mlghNM )
+
+
+## SANN method
+set.seed( 345 )
+mlSANN <- maxLik( llf, start = c( 0, 1 ), method = "SANN" )
+print( mlSANN )
+summary( mlSANN )
+activePar( mlSANN )
+AIC( mlSANN )
+coef( mlSANN )
+condiNumber( mlSANN )
+hessian( mlSANN )
+logLik( mlSANN )
+maximType( mlSANN )
+nIter( mlSANN )
+nObs( mlSANN )
+nParam( mlSANN )
+returnCode( mlSANN )
+returnMessage( mlSANN )
+# vcov( mlSANN )
+logLik( summary( mlSANN ) )
+all.equal( ml[-8], mlSANN )
+
+# with unused analytical gradients
+set.seed( 345 )
+mlgSANN <- maxLik( llf, gf, start = c( 0, 1 ), method = "SANN" )
+summary( mlgSANN )
+all.equal( mlSANN, mlgSANN )
+
+# with unused analytical gradients and Hessian
+set.seed( 345 )
+mlghSANN <- maxLik( llf, gf, hf, start = c( 0, 1 ), method = "SANN" )
+all.equal( mlgSANN, mlghSANN )
+
