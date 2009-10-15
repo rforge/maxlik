99a2be7104e5ccd2741f397fa6ecb0844d5e72da
otoomet <otoomet@af626d81-0c90-417d-8daa-7afe02cc634b>
2009-10-15 17:05:36 +0000

    coef names to vcov.maxLik
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/maxlik@1103 af626d81-0c90-417d-8daa-7afe02cc634b

--- pkg/R/vcov.maxLik.R
+++ pkg/R/vcov.maxLik.R
@@ -9,6 +9,7 @@ vcov.maxLik <- function(object, eigentol=1e-12, ...) {
    hessev <- abs(eigen(hess, symmetric=TRUE, only.values=TRUE)$values)
    varcovar <- matrix(0, nParam(object), nParam(object))
                            # this makes the fixed parameters to 0
+   rownames( varcovar ) <- colnames(varcovar ) <- names(coef(object))
    if(min(hessev) > (eigentol*max(hessev))) {
       ## If hessian is not singular, fill in the free parameter values
       varcovar[activePar,activePar] <- solve(-hessian(object)[activePar,activePar])
