233dca709524d76dc3458897c34af1ce85dabd49
arne <arne@af626d81-0c90-417d-8daa-7afe02cc634b>
2010-03-29 14:08:27 +0000

    function gradientS() in maxNM() is equal to the function with the same name in maxBFGS() now
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/maxlik@1131 af626d81-0c90-417d-8daa-7afe02cc634b

--- pkg/R/maxNM.R
+++ pkg/R/maxNM.R
@@ -71,7 +71,12 @@ maxNM <- function(fn, grad=NULL, hess=NULL,
          names( g ) <- names( start )
          return( g )
       }
-      g <- numericGradient(logLikFunc, theta, ...)
+      g <- match.call()
+      g[names(formals(sumt))] <- NULL
+      g[[1]] <- as.name("numericGradient")
+      names(g)[2] <- "t0"
+      g$f <- logLikFunc
+      g <- eval(g, sys.frame(sys.parent()))
       if(!is.null(dim(g))) {
          return(colSums(g))
       } else {
