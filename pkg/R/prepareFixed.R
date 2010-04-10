9ab33da84db38bad4628c7e1ac1abae6992fdb95
arne <arne@af626d81-0c90-417d-8daa-7afe02cc634b>
2010-04-10 22:29:51 +0000

    argument "fixed" is thoroughly tested in prepareFixed() now
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/maxlik@1212 af626d81-0c90-417d-8daa-7afe02cc634b

--- pkg/R/prepareFixed.R
+++ pkg/R/prepareFixed.R
@@ -7,14 +7,55 @@ prepareFixed <- function( start, activePar, fixed ) {
             warning("Both 'activePar' and 'fixed' specified.  'activePar' ignored")
          }
       }
-      activePar <- rep(TRUE, length(start))
-      names(activePar) <- names(start)
-      activePar[fixed] <- FALSE
+      if( is.logical( fixed ) ) {
+         if( length ( fixed ) != length( start ) || !is.null( dim( fixed ) ) ) {
+            stop( "if fixed parameters are specified using logical values,",
+               " argument 'fixed' must be a logical vector",
+               " with one element for each parameter",
+               " (number of elements in argument 'start')" )
+         }
+         activePar <- !fixed
+      } else if( is.numeric( fixed ) ) {
+         if( length ( fixed ) >= length( start ) || !is.null( dim( fixed ) ) ) {
+            stop( "if fixed parameters are specified using their positions,",
+               " argument 'fixed' must be a numerical vector",
+               " with less elements than the number of parameters",
+               " (number of elements in argument 'start'" )
+         } else if( min( fixed ) < 1 || max(fixed ) > length( start ) ) {
+            stop( "if fixed parameters are specified using their positions,",
+               " argument 'fixed' must have values between 1 and",
+               " the total number of parameter",
+               " (number of elements in argument 'start'" )
+         }
+         activePar <- ! c( 1:length( start ) ) %in% fixed
+      } else if( is.character( fixed ) ) {
+         if( length ( fixed ) >= length( start ) || !is.null( dim( fixed ) ) ) {
+            stop( "if fixed parameters are specified using their names,",
+               " argument 'fixed' must be a vector of character strings",
+               " with less elements than the number of parameters",
+               " (number of elements in argument 'start'" )
+         } else if( is.null( names( start ) ) ) {
+            stop( "if fixed parameters are specified using their names,",
+               " parameter names have to be specified in argument 'start'" )
+         } else if( any( ! names( fixed ) %in% names( start ) ) ) {
+            stop( "if fixed parameters are specified using their names,",
+               " all parameter names specified in argument 'fixed'",
+               " must be specified in argument 'start'" )
+         }
+         activePar <- ! names( start ) %in% fixed
+      } else {
+         stop( "argument 'fixed' must be either a logical vector,",
+            " a numeric vector, or a vector of character strings" )
+      }
    } else if(is.numeric(activePar)) {
       a <- rep(FALSE, nParam)
       a[activePar] <- TRUE
       activePar <- a
    }
-   names(activePar) <- names(start)
+   names( activePar ) <- names( start )
+   if( all( !activePar ) ){
+      stop( "At least one parameter must not be fixed",
+         " using argument 'fixed'" )
+   }
    return( !activePar )
 }
