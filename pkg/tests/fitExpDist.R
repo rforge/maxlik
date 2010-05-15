191bef1c4dba665cca34b804bae87fe28df07053
arne <arne@af626d81-0c90-417d-8daa-7afe02cc634b>
2010-05-15 07:06:01 +0000

    added tests for fitting an exponential distribution (e.g. estimation of an exponential duration model; taken from /tests/examples.R), i.e. tests with a model that has only one parameter
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/maxlik@1268 af626d81-0c90-417d-8daa-7afe02cc634b

---
+++ pkg/tests/fitExpDist.R
@@ -0,0 +1,107 @@
+## load the maxLik package
+library( maxLik )
+
+## fitting an exponential distribution by ML,
+## e.g. estimation of an exponential duration model
+
+# generate data
+set.seed( 4 )
+t <- rexp( 100, 2 )
+
+# log-likelihood function, gradient, and Hessian
+loglik <- function(theta) log(theta) - theta*t
+loglikSum <- function(theta) sum( log(theta) - theta*t )
+gradlik <- function(theta) 1/theta - t
+gradlikSum <- function(theta) sum( 1/theta - t )
+hesslik <- function(theta) -100/theta^2
+
+
+## NR estimation
+# Estimate with only function values
+ml <- maxLik( loglik, start = 1 )
+print( ml )
+summary( ml )
+print.default( ml )
+# log-likelihood value summed over all observations
+mlSum <- maxLik( loglikSum, start = 1 )
+all.equal( mlSum[], ml[-11] )
+
+# Estimate with analytic gradient
+mlg <- maxLik( loglik, gradlik, start = 1 )
+all.equal( mlg, ml )
+# gradient summed over all observations
+mlgSum <- maxLik( loglikSum, gradlikSum, start = 1 )
+all.equal( mlgSum[], mlg[-11] )
+
+# Estimate with analytic gradient and Hessian
+mlgh <- maxLik( loglik, gradlik, hesslik, start = 1 )
+all.equal( mlgh, mlg )
+
+
+## BHHH estimation
+# Estimate with only function values
+mlBhhh <- maxLik( loglik, start = 1, method = "BHHH" )
+print( mlBhhh )
+summary( mlBhhh )
+all.equal( mlBhhh[ -c( 5, 6, 10 ) ], ml[ -c( 5, 6, 10 ) ] )
+
+# Estimate with analytic gradient
+mlgBhhh <- maxLik( loglik, gradlik, start = 1, method = "BHHH" )
+all.equal( mlgBhhh, mlBhhh )
+
+# Estimate with analytic gradient and Hessian (unused during estimation)
+mlghBhhh <- maxLik( loglik, gradlik, hesslik, start = 1, method = "BHHH" )
+all.equal( mlghBhhh, mlgBhhh )
+
+## BFGS estimation
+# Estimate with only function values
+mlBfgs <- maxLik( loglik, start = 1, method = "BFGS" )
+print( mlBfgs )
+summary( mlBfgs )
+all.equal( mlBfgs[ -c( 5, 6, 9, 10, 11 ) ], ml[ -c( 5, 6, 9, 10 ) ] )
+# log-likelihood value summed over all observations
+mlSumBfgs <- maxLik( loglikSum, start = 1, method = "BFGS" )
+all.equal( mlSumBfgs[], mlBfgs[-12] )
+
+# Estimate with analytic gradient
+mlgBfgs <- maxLik( loglik, gradlik, start = 1, method = "BFGS" )
+all.equal( mlgBfgs, mlBfgs )
+# gradient summed over all observations
+mlgSumBfgs <- maxLik( loglikSum, gradlikSum, start = 1, method = "BFGS" )
+all.equal( mlgSumBfgs[], mlgBfgs[-12] )
+
+# Estimate with analytic gradient and Hessian (unused during estimation)
+mlghBfgs <- maxLik( loglik, gradlik, hesslik, start = 1, method = "BFGS" )
+all.equal( mlghBfgs, mlgBfgs )
+
+## NM estimation
+# Estimate with only function values
+mlNm <- maxLik( loglik, start = 1, method = "NM" )
+print( mlNm )
+summary( mlNm )
+all.equal( mlNm[ -c( 5, 6, 9, 10, 11 ) ], ml[ -c( 5, 6, 9, 10 ) ] )
+
+# Estimate with analytic gradient (unused during estimation)
+mlgNm <- maxLik( loglik, gradlik, start = 1, method = "NM" )
+all.equal( mlgNm, mlNm )
+
+# Estimate with analytic gradient and Hessian (both unused during estimation)
+mlghNm <- maxLik( loglik, gradlik, hesslik, start = 1, method = "NM" )
+all.equal( mlghNm, mlgNm )
+
+## SANN estimation
+# Estimate with only function values
+mlSann <- maxLik( loglik, start = 1, method = "SANN" )
+print( mlSann )
+summary( mlSann )
+all.equal( mlSann[ -c( 5, 6, 9, 10, 11 ) ], ml[ -c( 5, 6, 9, 10 ) ] )
+
+# Estimate with analytic gradient (unused during estimation)
+mlgSann <- maxLik( loglik, gradlik, start = 1, method = "SANN" )
+all.equal( mlgSann, mlSann )
+
+# Estimate with analytic gradient and Hessian (both unused during estimation)
+mlghSann <- maxLik( loglik, gradlik, hesslik, start = 1, method = "SANN" )
+all.equal( mlghSann, mlgSann )
+
+
