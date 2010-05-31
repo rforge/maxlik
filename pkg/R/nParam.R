87c303b4e2e415194f212e0f843ac3e2bdeadfad
arne <arne@af626d81-0c90-417d-8daa-7afe02cc634b>
2010-05-31 13:44:50 +0000

    deleted generic function nParam() including the "default" method and a method for objects of class "lm", because they have been moved to the "miscTools" package (the nParam() method for objects of class "maxim" is still in this package)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/maxlik@1278 af626d81-0c90-417d-8daa-7afe02cc634b

--- pkg/R/nParam.R
+++ pkg/R/nParam.R
@@ -1,13 +1,4 @@
 ## Return the #of parameters of model
-nParam <- function(x, free=FALSE, ...)
-    UseMethod("nParam")
-
-nParam.default <- function(x, ...)
-    x$param$nParam
-
-nParam.lm <- function(x, ...)
-    length(coefficients(x))
-
 nParam.maxim <- function(x, free=FALSE, ...) {
    if(free)
        sum(x$activePar)
