8fc6a660df2d0ebb8bd2870596eaa1cf99117eb3
arne <arne@af626d81-0c90-417d-8daa-7afe02cc634b>
2010-07-01 14:32:35 +0000

    logLikFunc() should preserve the attributes (e.g. "gradient" and "hesian") (also) if the sum over the individual observations is returned (argument "sumObs" is TRUE), and attribute "gradient" is summed over the observations if argument "sumObs" is TRUE
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/maxlik@1314 af626d81-0c90-417d-8daa-7afe02cc634b

--- pkg/R/logLikFunc.R
+++ pkg/R/logLikFunc.R
@@ -8,7 +8,22 @@ logLikFunc <- function(theta, fnOrig, gradOrig, hessOrig,
    result <- fnOrig( theta, ... )
 
    if( sumObs ) {
+      resultAttr <- attributes( result )
       result <- sum( result )
+      mostattributes( result ) <- attributes( result )
+      g <- attributes( result )$gradient
+      if( !is.null( g ) ) {
+         if( !is.null( dim( g ) ) ) {
+            g <- colSums(g)
+         } else if( length( theta ) == 1 && length( g ) > 1 ) {
+            g <- sum( g )
+         }
+         names( g ) <- names( theta )
+         if( !is.null( fixed ) ) {
+            g <- g[ !fixed ]
+         }
+         attributes( result )$gradient <- g
+      }
    }
 
    return( result )
