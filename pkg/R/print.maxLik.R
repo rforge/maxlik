55c2ac8c84a1bd5e6d42eb417d712d65890e3021
arne <arne@af626d81-0c90-417d-8daa-7afe02cc634b>
2010-07-05 12:46:39 +0000

    print.maxLik() now uses the activePar() method instead of x$activePar to determine the number of active parameters
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/maxlik@1336 af626d81-0c90-417d-8daa-7afe02cc634b

--- pkg/R/print.maxLik.R
+++ pkg/R/print.maxLik.R
@@ -4,7 +4,7 @@ print.maxLik <- function( x, ... ) {
    cat("Return code ", returnCode(x), ": ", returnMessage(x), "\n", sep="")
    if(!is.null(x$estimate)) {
       cat("Log-Likelihood:", x$maximum )
-      cat( " (", sum( x$activePar ), " free parameter(s))\n", sep = "" )
+      cat( " (", sum( activePar( x ) ), " free parameter(s))\n", sep = "" )
       cat("Estimate(s):", x$estimate, "\n" )
    }
 }
