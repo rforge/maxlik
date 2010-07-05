3e18cd4aca36d97853078dc18ac317328ff6ed27
arne <arne@af626d81-0c90-417d-8daa-7afe02cc634b>
2010-07-05 12:19:34 +0000

    nParam.maxim() now uses the activePar() method instead of x$activePar to determine tha number of active parameters
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/maxlik@1333 af626d81-0c90-417d-8daa-7afe02cc634b

--- pkg/R/nParam.R
+++ pkg/R/nParam.R
@@ -1,7 +1,7 @@
 ## Return the #of parameters of model
 nParam.maxim <- function(x, free=FALSE, ...) {
    if(free)
-       sum(x$activePar)
+       sum( activePar( x ) )
    else
        length( x$estimate )
 }
