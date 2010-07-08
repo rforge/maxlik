434e1c1b2e164f25e073a1992ad630a141a4b242
arne <arne@af626d81-0c90-417d-8daa-7afe02cc634b>
2010-07-08 15:08:43 +0000

    maxBHHH() now is simply a wrapper function to maxNR() with argument "bhhhHessian" set to TRUE: now a warning is also issued if maxBHHH() is called with gradients (or Hessian) provided both as argument and attribute; this modification changed some results obtained by the BHHH method very slightly and the results obtained by the BHHH method with equality constraints changed somewhat (I think that the new results are obtained in the correct/intended way)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/maxlik@1377 af626d81-0c90-417d-8daa-7afe02cc634b

--- pkg/R/maxBHHH.R
+++ pkg/R/maxBHHH.R
@@ -16,47 +16,13 @@ maxBHHH <- function(fn, grad=NULL, hess=NULL,
       checkFuncArgs( hess, argNames, "hess", "maxBHHH" )
    }
 
-   ## wrapper function for obtaining the log-likelihood value, gradient, and Hessian
-   func <- function(theta, ...) {
-      a <- fn(theta, ...)
-      if(!is.null(g <- attr(a, "gradient"))) {
-         ## gradient when it is supplied as attribute
-         checkBhhhGrad( g = g, theta = theta, analytic = TRUE )
-      } else if(!is.null(grad)) {
-         ## gradient supplied as function "grad"
-         g <- grad(theta, ...)
-         if( length( theta ) == 1 ) {
-            g <- matrix( g, ncol = 1 )
-         }
-         checkBhhhGrad( g = g, theta = theta, analytic = TRUE )
-      } else {
-         ## fall back to the numeric gradient
-         g <- numericGradient(fn, theta, ...)
-         checkBhhhGrad( g = g, theta = theta, analytic = FALSE )
-      }
-      attr( a, "gradient" ) <- g
-      attr( a, "hessian" ) <- - crossprod( g )
-
-      return( a )
-   }
-
    ## using the Newton-Raphson algorithm with BHHH method for Hessian
-   a <- maxNR(func, start=start,
+   a <- maxNR( fn=fn, grad = grad, hess = hess, start=start,
               iterlim=iterlim,
               print.level=print.level,
-              finalHessian = ( tolower( finalHessian ) == "bhhh" ),
+              finalHessian = finalHessian,
+              bhhhHessian = TRUE,
                ...)
-   if( tolower( finalHessian ) == "bhhh" ) {
-      attr( a$hessian, "type" ) <- "BHHH"
-   } else if( isTRUE( finalHessian ) ) {
-      a$hessian <- as.matrix( callWithoutSumt( a$estimate, "logLikHess",
-         fnOrig = fn, gradOrig = grad, hessOrig = hess, ... ) )
-   } else {
-       a$hessian <- NULL
-   }
-   if( !is.null( a$hessian ) ) {
-      rownames( a$hessian ) <- colnames( a$hessian ) <- names( a$estimate )
-   }
 
    a$type = "BHHH maximisation"
    invisible(a)
