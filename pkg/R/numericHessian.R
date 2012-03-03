1c48997d4e12e1eca5a64692addffb2c76d3b45c
otoomet <otoomet@af626d81-0c90-417d-8daa-7afe02cc634b>
2012-03-03 17:05:04 +0000

    numericNHessian checks if the function values are numeric scalars, and issues a warning if not.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/maxlik@1437 af626d81-0c90-417d-8daa-7afe02cc634b

--- pkg/R/numericHessian.R
+++ pkg/R/numericHessian.R
@@ -15,9 +15,36 @@ numericNHessian <- function( f, t0, eps=1e-6, fixed, ...) {
    ## Assume f() returns a scalar
    ## 
    ## fixed   calculate the Hessian only for the non-fixed parameters
+   warnMessage <- function(theta, value) {
+      ## issue a warning if the function value at theta is not a scalar
+      max.print <- 10
+      if(length(value) != 1) {
+         warnMsg <- "Function value at\n"
+         warnMsg <- c(warnMsg,
+                      paste(format(theta[seq(length=min(max.print,length(theta)))]),
+                             collapse=" "),
+                      "\n")
+         if(max.print < length(theta))
+             warnMsg <- c(warnMsg, "...\n")
+         warnMsg <- c(warnMsg, " =\n")
+         warnMsg <- c(warnMsg,
+                      paste(format(value[seq(length=min(max.print,length(value)))]),
+                                   collapse=" "),
+                      "\n")
+         if(max.print < length(value))
+             warnMsg <- c(warnMsg, "...\n")
+         warnMsg <- c(warnMsg, "but numeric Hessian only works on numeric scalars\n",
+                      "Component set to NA")
+         return(warnMsg)
+      }
+      if(!is.numeric(value))
+          stop("The function value must be a scalar")
+      return(NULL)
+   }
    f00 <- f( t0, ...)
-   if(length(f00) > 1) {
-      stop("Numeric Hessian only works for scalar functions")
+   if(!is.null(msg <- warnMessage(t0, f00))) {
+      warning(msg)
+      f00 <- NA
    }
    eps2 <- eps*eps
    N <- length( t0)
@@ -39,11 +66,22 @@ numericNHessian <- function( f, t0, eps=1e-6, fixed, ...) {
          t11[i] <- t11[i] + eps
          t11[j] <- t11[j] + eps
          f01 <- f( t01, ...)
+         if(!is.null(msg <- warnMessage(t01, f01))) {
+            warning(msg)
+            f01 <- NA
+         }
          f10 <- f( t10, ...)
+         if(!is.null(msg <- warnMessage(t10, f10))) {
+            warning(msg)
+            f10 <- NA
+         }
          f11 <- f( t11, ...)
+         if(!is.null(msg <- warnMessage(t11, f11))) {
+            warning(msg)
+            f11 <- NA
+         }
          H[i,j] <- ( f11 - f01 - f10 + f00)/eps2
       }
    }
    return( H )
 }
-
