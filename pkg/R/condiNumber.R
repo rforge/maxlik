39410ffc4cc55df2370ec4f1f0bd8baf2bd9ee76
arne <arne@af626d81-0c90-417d-8daa-7afe02cc634b>
2014-12-29 23:12:30 +0000

    further arguments to condiNumber.maxLik() are now passed to condiNumber.default() rather than to hessian()
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/maxlik@1491 af626d81-0c90-417d-8daa-7afe02cc634b

--- pkg/R/condiNumber.R
+++ pkg/R/condiNumber.R
@@ -29,4 +29,5 @@ condiNumber.default <- function(x, exact=FALSE, norm=FALSE, print.level=1, ...) 
 }
 
 condiNumber.maxLik <- function(x, ...)
-    condiNumber.default(hessian(x, ...)[activePar(x), activePar(x),drop=FALSE])
+    condiNumber.default( x = hessian(x)[activePar(x), activePar(x),drop=FALSE],
+       ... )
