36bd7368ae0e8e86c7cc420279798c0d66d3c442
otoomet <otoomet@af626d81-0c90-417d-8daa-7afe02cc634b>
2016-02-15 01:39:09 +0000

    maxNR documentation improved/updated wrt maxValue() getter. test/methods.R: corresponding test included
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/maxlik@1566 af626d81-0c90-417d-8daa-7afe02cc634b

--- pkg/tests/methods.R
+++ pkg/tests/methods.R
@@ -1,4 +1,5 @@
 library(maxLik)
+require(testthat)
 set.seed(0)
 
 ## Test standard methods for "lm"
@@ -11,10 +12,11 @@ print(stdEr(m))
 ## Test maxControl methods:
 set.seed(9)
 x <- rnorm(20)
-ll <- function(x) dnorm(x, log=TRUE)
+ll <- function(mu) dnorm(x, mean=mu, log=TRUE)
 for(method in c("NR", "BFGS", "BFGSR")) {
    cat("-- method", method, "--\n")
    m <- maxLik(ll, start=0, method=method, control=list(iterlim=1))
+   expect_equal(maxValue(m), -27.5157350664669, tolerance=0.001)
    cat("MaxControl structure:\n")
    show(maxControl(m))
 }
