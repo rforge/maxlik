d079ebb939a0f792e0f13747fc5459c909be40e9
rpruim <rpruim@af626d81-0c90-417d-8daa-7afe02cc634b>
2016-02-15 12:48:08 +0000

    Cleaning up use of S3 as per formum:42907
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/maxlik@1569 af626d81-0c90-417d-8daa-7afe02cc634b

--- pkg/R/stdEr.maxLik.R
+++ pkg/R/stdEr.maxLik.R
@@ -1,15 +1,15 @@
 
 stdEr.maxLik <- function(x, eigentol=1e-12, ...) {
-   if(!inherits(x, "maxLik"))
-       stop("'stdEr.maxLik' called on a non-'maxLik' object")
+   ## if(!inherits(x, "maxLik"))
+   ##    stop("'stdEr.maxLik' called on a non-'maxLik' object")
    ## Here we should actually coerce the object to a 'maxLik' object, dropping all the subclasses...
    ## Instead, we force the program to use maxLik-related methods
-   if(!is.null(vc <- vcov.maxLik(x, eigentol=eigentol))) {
+   if(!is.null(vc <- vcov(x, eigentol=eigentol))) {
       s <- sqrt(diag(vc))
-      names(s) <- names(coef.maxLik(x))
+      names(s) <- names(coef(x))
       return(s)
    }
+   # if vcov is not working, return NULL
    return(NULL)
-                           # if vcov is not working, return NULL
 }
 
