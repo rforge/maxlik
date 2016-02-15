b2f992890f7e783d00dd12662a32459e7cee7ac2
otoomet <otoomet@af626d81-0c90-417d-8daa-7afe02cc634b>
2016-02-15 03:22:42 +0000

    gradient method implemented, logLik gets df attribute, documentation of optimizers talks about getters
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/maxlik@1567 af626d81-0c90-417d-8daa-7afe02cc634b

--- pkg/R/logLik.maxLik.R
+++ pkg/R/logLik.maxLik.R
@@ -1,7 +1,13 @@
 ### Methods for accessing loglik value maximum likelihood estimates
 
-logLik.summary.maxLik <- function( object, ...)
-    object$loglik
+logLik.summary.maxLik <- function( object, ...) {
+   ll <- object$loglik
+   attr(ll, "df") <- sum(activePar(object))
+   ll
+}
 
-logLik.maxLik <- function( object, ...)
-    object$maximum
+logLik.maxLik <- function( object, ...) {
+   ll <- maxValue(object)
+   attr(ll, "df") <- sum(activePar(object))
+   ll
+}
--- pkg/tests/methods.R
+++ pkg/tests/methods.R
@@ -1,5 +1,6 @@
 library(maxLik)
 require(testthat)
+require(sandwich)
 set.seed(0)
 
 ## Test standard methods for "lm"
@@ -11,12 +12,16 @@ print(stdEr(m))
 
 ## Test maxControl methods:
 set.seed(9)
-x <- rnorm(20)
-ll <- function(mu) dnorm(x, mean=mu, log=TRUE)
+x <- rnorm(20, sd=2)
+ll <- function(par) dnorm(x, mean=par[1], sd=par[2], log=TRUE)
 for(method in c("NR", "BFGS", "BFGSR")) {
    cat("-- method", method, "--\n")
-   m <- maxLik(ll, start=0, method=method, control=list(iterlim=1))
-   expect_equal(maxValue(m), -27.5157350664669, tolerance=0.001)
+   m <- maxLik(ll, start=c(0, 2), method=method, control=list(iterlim=1))
+   expect_equal(maxValue(m), -41.35, tolerance=0.01)
+   expect_true(is.vector(gradient(m)), info="'gradient' returns a vector")
+   expect_equal(length(gradient(m)), 2, info="'gradient(m)' is of length 2")
+   expect_true(is.matrix(estfun(m)), info="'estfun' returns a matrix")
+   expect_equal(dim(estfun(m)), c(20,2), info="'estfun(m)' is 20x2 matrix")
    cat("MaxControl structure:\n")
    show(maxControl(m))
 }
