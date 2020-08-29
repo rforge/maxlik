8851aec0842bdfbba87e271880720c65b0700e5d
otoomet <otoomet@af626d81-0c90-417d-8daa-7afe02cc634b>
2020-08-29 23:05:54 +0000

    modified Arne's approach to methods test; fixed a few minor issues
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/maxlik@1651 af626d81-0c90-417d-8daa-7afe02cc634b

--- pkg/tests/methods.R
+++ pkg/tests/methods.R
@@ -75,8 +75,5 @@ X <- matrix(rnorm(20*15), 20, 15)
 beta <- rep(1, ncol(X))
 y <- X %*% beta + rnorm(20, sd=0.3)
 m <- maxNR(loglik, gradlik, start=rep(1, ncol(X)), iterlim=1)
-options(digits=2)
-# rounding of the gradients so that minor difference in different computers
-# do not result in differences in the output
-m$gradient <- round( m$gradient, 6 )
-print(summary(m, hessian=TRUE), max.rows=4, max.cols=2)
+print(summary(m, hessian=TRUE), max.rows=4, max.cols=2, digits=1)
+                           # digits=1: very crude rounding but we want just to test the lines
