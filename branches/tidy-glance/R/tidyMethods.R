0a72a647826e06ca4ea01a837ace95fc95c6cab1
otoomet <otoomet@af626d81-0c90-417d-8daa-7afe02cc634b>
2021-04-09 05:11:54 +0000

    tidy and glance patch by David Hugh-Jones applied and incorporated
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/maxlik@1706 af626d81-0c90-417d-8daa-7afe02cc634b

---
+++ branches/tidy-glance/R/tidyMethods.R
@@ -0,0 +1,33 @@
+
+require_tibble_package <- function () {
+  if (! requireNamespace("tibble", quietly = TRUE)) {
+    stop("The `tibble` package must be installed to use tidy() or glance() methods")
+  }
+}
+
+
+tidy.maxLik <- function (x,  ...) {
+  require_tibble_package()
+  
+  s <- summary(x)
+  ret <- tibble::as_tibble(s$estimate, rownames = "term")
+  colnames(ret) <- c("term", "estimate", "std.error", "statistic", "p.value")
+  
+  ret
+}
+
+
+glance.maxLik <- function (x, ...) {
+  require_tibble_package()
+
+  ll <- logLik(x)
+  nobs <- tryCatch(nObs(x), error = NA)
+  ret <- tibble::tibble(
+           df     = attr(ll, "df"),
+           logLik = as.numeric(ll),
+           AIC    = AIC(x),
+           nobs   = nobs
+         )
+  
+  ret
+}
